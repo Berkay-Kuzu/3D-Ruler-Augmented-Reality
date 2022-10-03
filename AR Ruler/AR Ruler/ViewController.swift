26b3b56864c59ebd4e940558ba7194f0ddcb6734
Berkay Kuzu <92036779+Berkay-Kuzu@users.noreply.github.com>
2022-10-03 10:39:32 +0300

    Add files via upload

---
+++ AR Ruler/AR Ruler/ViewController.swift
@@ -0,0 +1,126 @@
+//
+//  ViewController.swift
+//  AR Ruler
+//
+//  Created by Berkay Kuzu on 27.08.2022.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+    
+    @IBOutlet var sceneView: ARSCNView!
+    
+    var dotNodes = [SCNNode]() // We keep all dotNodes here!
+    var textNode = SCNNode()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if dotNodes.count >= 2 {
+            for dot in dotNodes {
+                dot.removeFromParentNode()
+            }
+            dotNodes = [SCNNode]() // we clear the SCNode array, now we have fresh one.
+        }
+        
+        if let touchLocation = touches.first?.location(in: sceneView) {
+            let hitTestResults = sceneView.hitTest(touchLocation, types: .featurePoint)
+            
+            if let hitResult = hitTestResults.first { // 3D Location in the real world.
+                addDo(at: hitResult)
+            }
+        }
+        
+    }
+    
+    func addDo(at hitResult: ARHitTestResult) {
+        let dotGeometry = SCNSphere(radius: 0.005)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+        
+        dotGeometry.materials = [material] // assigning the material!
+        
+        let dotNode = SCNNode(geometry: dotGeometry)
+        
+        dotNode.position = SCNVector3(hitResult.worldTransform.columns.3.x,
+                                      hitResult.worldTransform.columns.3.y,
+                                      hitResult.worldTransform.columns.3.z)
+        
+        sceneView.scene.rootNode.addChildNode(dotNode)
+        
+        dotNodes.append(dotNode)
+        
+        if dotNodes.count >= 2 {
+            calculate()
+        }
+    }
+    
+    func calculate() {
+        let start = dotNodes[0] // This is our starting position where we want start measuring from.
+        let end = dotNodes[1] // This is our ending position where we want to measure.
+        
+        print(start.position)
+        print(end.position)
+        
+        let a = end.position.x - start.position.x
+        let b = end.position.y - start.position.y
+        let c = end.position.z - start.position.z
+        
+        let distance = sqrt(pow(a, 2) + pow(b, 2) + pow(c, 2))
+        
+       // print(abs(distance)) // By using abs(), we removed all the sign, we reached the distance!
+        
+        updateText(text: "\(abs(distance))", atPosition: end.position)
+        
+       // distance = √((x2-x1)^2 + (y2-y1)^2 + (x2-x1)^2)
+    }
+    
+    func updateText(text: String, atPosition position: SCNVector3) {
+        //Inside here, we create a text geometry:
+        
+        textNode.removeFromParentNode()
+        
+        let textGeometry = SCNText(string: text, extrusionDepth: 1.0)
+        
+        textGeometry.firstMaterial?.diffuse.contents = UIColor.red
+        
+        textNode = SCNNode(geometry: textGeometry)
+        
+        textNode.position = SCNVector3(position.x, position.y + 0.01, position.z)
+        
+        textNode.scale = SCNVector3(0.01, 0.01, 0.01)
+        
+        sceneView.scene.rootNode.addChildNode(textNode)
+        
+    }
+    
+}
